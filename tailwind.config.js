2369f0561ac5f14951d845f24b8ec0ea87b24336
Ferovac <85068658+Jure-Rajcic@users.noreply.github.com>
2024-11-04 15:23:56 +0100

    navbar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  prefix: "tw-",
+  important: true,
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        // Bootstrap breakpoints
+        'xs': '0px',          // Bootstrap does not have a specific xs class
+        'sm': '576px',        // Small screens
+        'md': '768px',        // Medium screens
+        'lg': '992px',        // Large screens
+        'xl': '1200px',       // Extra large screens
+        'xxl': '1400px',      // Extra extra large screens (Bootstrap 5)
+      },
+    },
   },
   plugins: [],
 }
