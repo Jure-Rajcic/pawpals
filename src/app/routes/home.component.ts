2f798cafdff337d5658724375ae222ec08fdd747
Ferovac <85068658+Jure-Rajcic@users.noreply.github.com>
2024-11-06 01:09:39 +0100

    made fade slide in effect'

--- src/app/routes/home.component.ts
+++ src/app/routes/home.component.ts
@@ -1,29 +1,68 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { trigger, state, style, animate, transition, query, group } from '@angular/animations';
 import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   template: `
-    <p class="tw-text-center tw-uppercase">--- Creative Marketing Agency</p>
-    <div class="tw-my-8">
+    <p class="tw-text-center tw-uppercase fade-element">--- Creative Marketing Agency</p>
+    <div class="tw-my-8 fade-element">
       <h1 class="tw-text-center tw-text-8xl tw-font-montserrat tw-font-bold">Digital. Brand.</h1>
       <h1 class="type-writer tw-text-center tw-text-8xl tw-font-montserrat tw-font-bold"></h1>
     </div>
-    <p class="tw-text-center tw-uppercase">We help brands and companies stand out in the digital age.</p>
-    <h1 [@fadeIn] class="fade-element">This is fade element</h1>
+    <p class="tw-text-center tw-uppercase fade-element">We help brands and companies stand out in the digital age.</p>
   `,
+  styles: [`
 
+    .fade-element {
+      opacity: 0;
+    }
+
+    .slide-in {
+      animation: slide-in 1s ease-out;
+    }
+
+    @keyframes slide-in {
+      0% {
+        transform: translateX(-5%); /* Start off-screen to the left */
+        opacity: 0; /* Start fully transparent */
+      }
+      75% {
+        opacity: 10; /* Fade in */
+      }
+      100% {
+        transform: translateX(0); /* End at normal position */
+        opacity: 1; /* End fully visible */
+      }
+    }
+  `],
 })
-export class HomeComponent implements AfterViewInit {
 
+export class HomeComponent implements AfterViewInit {
   ngAfterViewInit() {
+    this._setFadeSlide();
     this._setTypewriter();
   }
 
-  _setTypewriter() {
+  private _setFadeSlide() {
+    const fadeElements = document.querySelectorAll('.fade-element');
+    const fadeElementArray = Array.from(fadeElements);
+    console.log(fadeElementArray);
+
+      fadeElementArray.forEach((element, index) => {
+        setTimeout(() => {
+          element?.classList.add('slide-in');
+          element?.classList.remove('fade-element');
+        }, 750 * index); // Each element waits an additional second based on its index
+      });
+      
+
+  }
+
+  private _setTypewriter() {
     const target = document.querySelector('.type-writer');
-    
+
     if (!target) {
       console.warn('Typewriter target element not found');
       return;
